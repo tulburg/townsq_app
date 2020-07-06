d747b4e08d1ae1ba756b76c53566224c0fef875b
Tolu Oluwagbemi <tulburg2@gmail.com>
2020-07-06 08:57:31 +0200

    Initial Commit

---
+++ Kast/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kast
+//
+//  Created by Tolu Oluwagbemi on 06/07/2020.
+//  Copyright © 2020 Tolu Oluwagbemi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Kast/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kast
+//
+//  Created by Tolu Oluwagbemi on 06/07/2020.
+//  Copyright © 2020 Tolu Oluwagbemi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
